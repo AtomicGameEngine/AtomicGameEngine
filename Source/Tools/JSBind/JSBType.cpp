71f50ac10b61cbc579ca8145c67ea1066349d716
Josh Engebretson <josh@galaxyfarfaraway.com>
2015-02-12 17:12:20 -0800

    Updating repository link

--- Source/Tools/JSBind/JSBType.cpp
+++ Source/Tools/JSBind/JSBType.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2014-2015, THUNDERBEAST GAMES LLC All rights reserved
 // Please see LICENSE.md in repository root for license information
-// https://github.com/AtomicGameEngine/AtomicEditor
+// https://github.com/AtomicGameEngine/AtomicGameEngine
 
 #include <Atomic/Atomic.h>
 #include "JSBType.h"
