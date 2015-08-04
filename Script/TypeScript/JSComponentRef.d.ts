1e7ffefa4de00b3cdae3df4fa819e7da1b9869c3
Matt Benic <mattbenic@gmail.com>
2015-08-04 17:53:54 +0200

    A manually created definitions file for items not yet automatically generated by JSBind (mostly global values from explicit duktape CPP and AtomicGame added in js). Definition to be included in JS components to allow their self to be redeclared, and modified lib.d.ts to facilitate this.
    (cherry picked from commit a190372c352766c04134d99bc738e4b24dc524fd)
    
    (cherry picked from commit a190372c352766c04134d99bc738e4b24dc524fd)

---
+++ Script/TypeScript/JSComponentRef.d.ts
@@ -0,0 +1,5 @@
+// To be included in JSComponent scripts for common includes
+/// <reference path="./AtomicExtended.d.ts" />
+
+// Redeclare self as a JSComponent
+declare var self: Atomic.JSComponent;
