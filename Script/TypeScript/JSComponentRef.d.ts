84da5a2c18cd6bde0d43aa803673a5f45c2425fb
Matt Benic <mattbenic@gmail.com>
2015-08-12 14:12:15 +0200

    These are no longer necessary, since JSComponents can just be written as TS classes extending Atomic.JSComponent and "this" can be used where "self" would have before.

--- Script/TypeScript/JSComponentRef.d.ts
+++
@@ -1,5 +0,0 @@
-// To be included in JSComponent scripts for common includes
-/// <reference path="./AtomicExtended.d.ts" />
-
-// Redeclare self as a JSComponent
-declare var self: Atomic.JSComponent;
