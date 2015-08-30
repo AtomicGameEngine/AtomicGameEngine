a0faa8acd0a295fce97199c6a3cb8061eec867d8
Josh Engebretson <josh@galaxyfarfaraway.com>
2015-08-29 21:11:08 -0700

    Shutdown JS vm at application stop

--- Source/AtomicPlayer/Application/AtomicPlayer.cpp
+++ Source/AtomicPlayer/Application/AtomicPlayer.cpp
@@ -147,7 +147,16 @@ void AtomicPlayerApp::Start()
 
 void AtomicPlayerApp::Stop()
 {
+
+    vm_ = 0;
+    context_->RemoveSubsystem<Javascript>();
+    // make sure JSVM is really down and no outstanding refs
+    // as if not, will hold on engine subsystems, which is bad
+    assert(!JSVM::GetJSVM(0));
+
     Application::Stop();
+
+
 }
 
 void AtomicPlayerApp::HandleScriptReloadStarted(StringHash eventType, VariantMap& eventData)
