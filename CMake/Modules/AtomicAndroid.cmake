2a14955ccb107d8b9a42bf525ed10c5ffd7fe5d3
Josh Engebretson <josh@galaxyfarfaraway.com>
2015-09-02 20:25:11 -0700

    Add UI to Android/iOS builds

--- CMake/Modules/AtomicAndroid.cmake
+++ CMake/Modules/AtomicAndroid.cmake
@@ -2,7 +2,7 @@
 set (JAVASCRIPT_BINDINGS_PLATFORM "ANDROID")
 
 add_definitions(-DATOMIC_PLATFORM_ANDROID)
-add_definitions(-DATOMIC_OPENGL -DKNET_UNIX)
+add_definitions(-DATOMIC_OPENGL -DKNET_UNIX -DATOMIC_TBUI)
 
 set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fstack-protector")
 set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-invalid-offsetof -std=gnu++0x -fstack-protector")
