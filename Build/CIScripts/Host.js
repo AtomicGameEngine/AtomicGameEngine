8cd1eaf849173d79789e0a848f879066576c4b7e
Josh Engebretson <josh@galaxyfarfaraway.com>
2015-10-29 06:58:11 -0700

    Adding CI editor data generator task

--- Build/CIScripts/Host.js
+++ Build/CIScripts/Host.js
@@ -1,11 +1,14 @@
 var os = require('os');
 
 if (os.platform() == "darwin") {
+
   module.exports = require("./HostMac");
   require("./BuildMac");
   require("./BuildIOS");
   require("./BuildWeb");
   require("./BuildAndroid");
+  require("./GenEditorData");
+
 } else if (os.platform() == "win32") {
   module.exports = require("./HostWindows");
   require("./BuildWindows");
