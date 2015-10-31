80e9a3c9b775c34b37cdb7f967bf23aebbadcf6a
Josh Engebretson <josh@galaxyfarfaraway.com>
2015-10-31 07:39:10 -0700

    Working on build cleanups

--- Build/Scripts/Host.js
+++ Build/Scripts/Host.js
@@ -2,4 +2,6 @@ var os = require('os');
 
 if (os.platform() == "win32") {
   module.exports = require("./HostWindows");
+} else if (os.platform() == "darwin") {
+  module.exports = require("./HostMac");
 }
