923deca52ce91c25acc38892b1ce05192dffe410
Josh Engebretson <josh@galaxyfarfaraway.com>
2015-11-27 09:52:25 -0800

    Fix stanhull return

--- Source/ThirdParty/StanHull/hull.cpp
+++ Source/ThirdParty/StanHull/hull.cpp
@@ -2587,7 +2587,7 @@ int overhull(Plane *planes,int planes_count,float3 *verts, int verts_count,int m
 			 float3 *&verts_out, int &verts_count_out,  int *&faces_out, int &faces_count_out ,float inflate)
 {
 	int i,j;
-	if(verts_count <4) return NULL;
+    if(verts_count <4) return 0;
 	maxplanes = Min(maxplanes,planes_count);
 	float3 bmin(verts[0]),bmax(verts[0]);
 	for(i=0;i<verts_count;i++) 
