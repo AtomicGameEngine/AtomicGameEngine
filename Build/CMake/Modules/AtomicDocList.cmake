d9e0cc3f7ccdc6738be48371aa8fa543d58512b5
ctrauma <scott@bitwise.ninja>
2015-12-19 11:39:52 -0800

    fix doc list

--- Build/CMake/Modules/AtomicDocList.cmake
+++ Build/CMake/Modules/AtomicDocList.cmake
@@ -1,7 +1,7 @@
 # create target to generate docs with clang based doc tool
-# NB: pretty brute force ATM
-if(EXISTS "/usr/local/bin/cldoc")
-   add_custom_target(AtomicEngineDocs 
+find_program(CLDOC cldoc)
+if(CLDOC)
+   add_custom_target(AtomicEngineDocs
     DEPENDS AtomicEditorDocs AtomicDocs
     )
 endif()
