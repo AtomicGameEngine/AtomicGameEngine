6d83efd9b95861d4c86d0c7784cc3d1efb685158
Josh Engebretson <josh@galaxyfarfaraway.com>
2016-01-17 10:48:24 -0800

    Refreshing Duktape for Regex updates (Git commit: 1a088177a2b5323419376a579d4ddc7a1d316489)

--- Source/ThirdParty/Duktape/duktape.h
+++ Source/ThirdParty/Duktape/duktape.h
@@ -1,12 +1,12 @@
 /*
- *  Duktape public API for Duktape 1.3.99.
+ *  Duktape public API for Duktape 1.4.99.
  *  See the API reference for documentation on call semantics.
  *  The exposed API is inside the DUK_API_PUBLIC_H_INCLUDED
  *  include guard.  Other parts of the header are Duktape
  *  internal and related to platform/compiler/feature detection.
  *
- *  Git commit 5c863edb9d3186e8db2417dcd1ac9a2b508f3023 (v1.3.0-431-g5c863ed).
- *  Git branch master.
+ *  Git commit 1a088177a2b5323419376a579d4ddc7a1d316489 (v1.4.0-50-g1a08817-dirty).
+ *  Git branch HEAD.
  *
  *  See Duktape AUTHORS.rst and LICENSE.txt for copyright and
  *  licensing information.
@@ -20,7 +20,7 @@
  *  
  *  (http://opensource.org/licenses/MIT)
  *  
- *  Copyright (c) 2013-2015 by Duktape authors (see AUTHORS.rst)
+ *  Copyright (c) 2013-2016 by Duktape authors (see AUTHORS.rst)
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -109,6 +109,8 @@
  *  * Michael Drake (https://github.com/tlsa)
  *  * https://github.com/chris-y
  *  * Laurent Zubiaur (https://github.com/lzubiaur)
+ *  * Ole Andr\u00e9 Vadla Ravn\u00e5s (https://github.com/oleavr)
+ *  * Julien Hamaide (https://github.com/crazyjul)
  *  
  *  If you are accidentally missing from this list, send me an e-mail
  *  (``sami.vaarala@iki.fi``) and I'll fix the omission.
@@ -213,16 +215,16 @@ struct duk_number_list_entry {
  * have 99 for patch level (e.g. 0.10.99 would be a development version
  * after 0.10.0 but before the next official release).
  */
-#define DUK_VERSION                       10399L
+#define DUK_VERSION                       10499L
 
 /* Git commit, describe, and branch for Duktape build.  Useful for
  * non-official snapshot builds so that application code can easily log
  * which Duktape snapshot was used.  Not available in the Ecmascript
  * environment.
  */
-#define DUK_GIT_COMMIT                    "5c863edb9d3186e8db2417dcd1ac9a2b508f3023"
-#define DUK_GIT_DESCRIBE                  "v1.3.0-431-g5c863ed"
-#define DUK_GIT_BRANCH                    "master"
+#define DUK_GIT_COMMIT                    "1a088177a2b5323419376a579d4ddc7a1d316489"
+#define DUK_GIT_DESCRIBE                  "v1.4.0-50-g1a08817-dirty"
+#define DUK_GIT_BRANCH                    "HEAD"
 
 /* Duktape debug protocol version used by this build. */
 #define DUK_DEBUG_PROTOCOL_VERSION        1
