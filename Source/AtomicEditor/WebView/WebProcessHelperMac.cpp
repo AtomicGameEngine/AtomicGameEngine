341ada321433c5626f0cfcf643fda13be3bc3202
Josh Engebretson <josh@galaxyfarfaraway.com>
2016-01-24 11:41:30 -0800

    Use WebMain for web helper executable on OS X

--- Source/AtomicEditor/WebView/WebProcessHelperMac.cpp
+++ Source/AtomicEditor/WebView/WebProcessHelperMac.cpp
@@ -2,31 +2,10 @@
 // reserved. Use of this source code is governed by a BSD-style license that can
 // be found in the LICENSE file.
 
-#include <AtomicWebView/Internal/WebAppOther.h>
-#include <AtomicWebView/Internal/WebAppRenderer.h>
-
-using namespace Atomic;
-
-#include "include/cef_app.h"
+#include <AtomicWebView/AtomicWebView.h>
 
 // Entry point function for sub-processes.
 int main(int argc, char* argv[])
 {
-    // Provide CEF with command-line arguments.
-    CefMainArgs main_args(argc, argv);
-
-    // Parse command-line arguments.
-    CefRefPtr<CefCommandLine> command_line = CefCommandLine::CreateCommandLine();
-    command_line->InitFromArgv(argc, argv);
-
-    // Create a ClientApp of the correct type.
-    CefRefPtr<CefApp> app;
-    WebApp::ProcessType process_type = WebApp::GetProcessType(command_line);
-    if (process_type == WebApp::RendererProcess)
-        app = new WebAppRenderer();
-    else if (process_type == WebApp::OtherProcess)
-        app = new WebAppOther();
-
-    // Execute the secondary process.
-    return CefExecuteProcess(main_args, app, NULL);
+    return Atomic::WebMain(argc, argv);
 }
