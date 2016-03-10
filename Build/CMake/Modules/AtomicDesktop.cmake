fd0b8e2c0e2fce22b33303133f2a38380445a78a
Cedric Seehausen <cedddy@gmail.com>
2016-03-10 22:43:30 +0100

    Added Status messages & checking if git is in path

--- Build/CMake/Modules/AtomicDesktop.cmake
+++ Build/CMake/Modules/AtomicDesktop.cmake
@@ -13,13 +13,35 @@ if (EXISTS ${CMAKE_SOURCE_DIR}/Submodules/CEF)
     file(GLOB RESULT ${CMAKE_SOURCE_DIR}/Submodules/CEF)
     list(LENGTH ${RESULT} RES_LEN)
     if(RES_LEN EQUAL 0)
-     # DIR is empty, so init the submodule and checkout master
-     execute_process(COMMAND git submodule update --init --remote)
-    endif()
+     message(STATUS "Trying to initalise CEF submodule")
 
-    set(ATOMIC_WEBVIEW TRUE)
-    add_definitions( -DATOMIC_WEBVIEW )
-    include("AtomicWebView")
+     set(PATH_STRING $ENV{PATH})
+     string(REPLACE ":" ";" PATHLIST ${PATH_STRING})
+
+     set(GIT_FOUND false)
+     foreach(cpath ${PATHLIST})
+      if (EXISTS ${cpath}/git)
+       set(GIT_FOUND true)
+       break()
+      endif()
+     endforeach(cpath)
+
+     # DIR is empty, so init the submodule and checkout master
+     if(GIT_FOUND)
+      execute_process(COMMAND git submodule update --init --remote)
+
+      set(ATOMIC_WEBVIEW TRUE)
+      add_definitions( -DATOMIC_WEBVIEW )
+      include("AtomicWebView")
+     else ()
+      message(STATUS "Could not find git in your Path please install git to enable WebView")
+     endif(GIT_FOUND)
+
+    else ()
+     set(ATOMIC_WEBVIEW TRUE)
+     add_definitions( -DATOMIC_WEBVIEW )
+     include("AtomicWebView")
+    endif()
 endif()
 
 include(AtomicNET)
