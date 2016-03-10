e06b4362d5c59d1449b4702513f9a2aa685a3a6e
Cedric Seehausen <cedddy@gmail.com>
2016-03-10 20:58:47 +0100

    Auto load submodules fix #687

--- Build/CMake/Modules/AtomicDesktop.cmake
+++ Build/CMake/Modules/AtomicDesktop.cmake
@@ -9,6 +9,14 @@ set (ATOMIC_LINK_LIBRARIES ${ATOMIC_LINK_LIBRARIES} LibCpuId SQLite)
 
 # Check whether the CEF submodule is available
 if (EXISTS ${CMAKE_SOURCE_DIR}/Submodules/CEF)
+    #Check if CEF got pulled
+    file(GLOB RESULT ${CMAKE_SOURCE_DIR}/Submodules/CEF)
+    list(LENGTH ${RESULT} RES_LEN)
+    if(RES_LEN EQUAL 0)
+     # DIR is empty, so init the submodule and checkout master
+     execute_process(COMMAND git submodule update --init --remote)
+    endif()
+
     set(ATOMIC_WEBVIEW TRUE)
     add_definitions( -DATOMIC_WEBVIEW )
     include("AtomicWebView")
