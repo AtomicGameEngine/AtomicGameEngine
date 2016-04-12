0224421939248f09ce57918195850a47bc4de20b
Matt Benic <mattbenic@gmail.com>
2016-04-12 16:01:34 +0200

    Fixed incorrect case in reference

--- Script/TypeScript/AtomicWork.d.ts
+++ Script/TypeScript/AtomicWork.d.ts
@@ -2,7 +2,7 @@
 /// <reference path="ToolCore.d.ts" />
 /// <reference path="Editor.d.ts" />
 /// <reference path="AtomicPlayer.d.ts" />
-/// <reference path="AtomicNet.d.ts" />
+/// <reference path="AtomicNET.d.ts" />
 
 declare module Atomic {
 
