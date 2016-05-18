6df35ebdf59c42d9a128d1c406b41a11b1aac4c6
weinand <weinanvv@gmail.com>
2016-05-18 09:57:06 +0200

    - Added zooming feature to the orthograpic view by using the mousewheel.
    - Refined some coding.

--- Source/AtomicEditor/Editors/SceneEditor3D/SceneView3D.cpp
+++ Source/AtomicEditor/Editors/SceneEditor3D/SceneView3D.cpp
@@ -187,6 +187,11 @@ void SceneView3D::MoveCamera(float timeStep)
     // Mouse sensitivity as degrees per pixel
     const float MOUSE_SENSITIVITY = 0.2f;
 
+    // Orthographic zoom settings
+    float currCameraZoom = camera_->GetZoom();
+    const float ZOOM_INCREMENT = 0.1f;
+    const float DEFAULT_ZOOM = 1.0f;
+
     if (shiftDown)
         MOVE_SPEED *= 3.0f;
 
@@ -284,6 +289,16 @@ void SceneView3D::MoveCamera(float timeStep)
         cameraNode_->SetWorldPosition(pos);
 
     }
+
+    if (camera_->IsOrthographic() && mouseInView && !orbitting)
+    {
+        if (input->GetMouseMoveWheel() > 0)
+            camera_->SetZoom(currCameraZoom + ZOOM_INCREMENT);
+        if (input->GetMouseMoveWheel() < 0)
+            camera_->SetZoom(currCameraZoom - ZOOM_INCREMENT);
+    }
+    else
+        camera_->SetZoom(DEFAULT_ZOOM);
 }
 
 void SceneView3D::SnapCameraToView(int snapView)
@@ -346,35 +361,19 @@ void SceneView3D::SelectView()
     if (MouseInView())
     {
         if (input->GetKeyPress(KEY_1))
-        {
             snapView = 1;
-            SetPerspectiveCameraPosition();
-        }
         if (input->GetKeyPress(KEY_2))
-        {
             snapView = 2;
-            SetPerspectiveCameraPosition();
-        }
         if (input->GetKeyPress(KEY_3))
-        {
             snapView = 3;
-            SetPerspectiveCameraPosition();
-        }
         if (input->GetKeyPress(KEY_4))
-        {
             snapView = 4;
-            SetPerspectiveCameraPosition();
-        }
         if (input->GetKeyPress(KEY_5))
-        {
             snapView = 5;
-            SetPerspectiveCameraPosition();
-        }
         if (input->GetKeyPress(KEY_6))
-        {
             snapView = 6;
-            SetPerspectiveCameraPosition();
-        }
+
+        SetPerspectiveCameraPosition();
 
         if (input->GetKeyPress(KEY_P))
         {
