22f17b6719585b18d5dd86590de501bd811c9839
Josh Engebretson <josh@galaxyfarfaraway.com>
2016-06-23 17:25:29 -0700

    Build XCode project directly when using CMake_XCode.sh

--- CMake_XCode.sh
+++ CMake_XCode.sh
@@ -1,2 +1,4 @@
-#!/bin/sh
-./Build/Mac/node/node ./Build/node_modules/jake/bin/cli.js -f ./Build/Scripts/Bootstrap.js build:genxcode --trace
+#!/usr/bin/env bash
+SOURCE=$(cd ${0%/*}; pwd)
+cmake -E make_directory ../AtomicGameEngine-XCode && cmake -E chdir ../AtomicGameEngine-XCode cmake "$SOURCE" -G Xcode
+echo "XCode project written to ../AtomicGameEngine-XCode"
