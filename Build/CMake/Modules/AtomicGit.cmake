4920e5f7dc72bf6ab564fd321f8a629e34065ca5
Josh Engebretson <josh@galaxyfarfaraway.com>
2016-10-26 12:29:32 -0700

    Inform user of submodule update

--- Build/CMake/Modules/AtomicGit.cmake
+++ Build/CMake/Modules/AtomicGit.cmake
@@ -6,6 +6,8 @@ if (NOT EXISTS ${CMAKE_SOURCE_DIR}/Submodules/CEF/.git OR NOT EXISTS ${CMAKE_SOU
 
     if(GIT_FOUND)
 
+        message ("\n\nUpdating submodules, please wait...\n\n")
+
         execute_process( COMMAND ${GIT_EXECUTABLE} submodule update --init
                          WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
                          RESULT_VARIABLE ATOMIC_GIT_STATUS )
