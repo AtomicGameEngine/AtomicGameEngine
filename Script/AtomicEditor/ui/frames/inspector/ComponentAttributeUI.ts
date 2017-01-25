fd8c0321fde4fe06fc770df2aa37c30e0be805fe
Josh Engebretson <josh@galaxyfarfaraway.com>
2017-01-25 07:46:27 -0800

    Fix for Skybox material assignment

--- Script/AtomicEditor/ui/frames/inspector/ComponentAttributeUI.ts
+++ Script/AtomicEditor/ui/frames/inspector/ComponentAttributeUI.ts
@@ -496,4 +496,5 @@ class SubmeshListAttributeEdit extends AttributeInfoEdit {
 
 AttributeInfoEdit.registerCustomAttr("AnimatedModel", "Material", SubmeshListAttributeEdit);
 AttributeInfoEdit.registerCustomAttr("StaticModel", "Material", SubmeshListAttributeEdit);
+AttributeInfoEdit.registerCustomAttr("Skybox", "Material", SubmeshListAttributeEdit);
 AttributeInfoEdit.registerCustomAttr("Light", "CSM Splits", LightCascadeAttributeEdit);
