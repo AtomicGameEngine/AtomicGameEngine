50a7a5a117f59cd1c4254b9d75e8a2edd4ea7921
Rokas Kupstys <rokups@zoho.com>
2017-04-12 09:52:53 +0300

    Prevents newly subscribed handlers from executing on very this frame. Fixes E_KEYUP causing FeatureSamples to exit instead of returning to menu.

--- Source/Atomic/Core/Context.cpp
+++ Source/Atomic/Core/Context.cpp
@@ -75,12 +75,27 @@ void EventReceiverGroup::EndSendEvent()
 
         dirty_ = false;
     }
+
+    // ATOMIC BEGIN
+    if (inSend_ == 0 && bufferedReceivers_.Size())
+    {
+        receivers_.Push(bufferedReceivers_);
+        bufferedReceivers_.Clear();
+    }
+    // ATOMIC END
 }
 
 void EventReceiverGroup::Add(Object* object)
 {
     if (object)
-        receivers_.Push(object);
+    {
+        // ATOMIC BEGIN
+        if (inSend_)
+            bufferedReceivers_.Push(object);
+        else
+        // ATOMIC END
+            receivers_.Push(object);
+    }
 }
 
 void EventReceiverGroup::Remove(Object* object)
