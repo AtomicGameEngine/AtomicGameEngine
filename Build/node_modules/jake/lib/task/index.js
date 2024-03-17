8f8ffd88fed6fe28920faff5236522340a360018
Ruben Gomes <rbnpontes@gmail.com>
2024-03-17 02:03:56 -0300

    feat: remove node_modules

--- Build/node_modules/jake/lib/task/index.js
+++
@@ -1,9 +0,0 @@
-
-var Task = require('./task').Task
-  , FileTask = require('./file_task').FileTask
-  , DirectoryTask = require('./directory_task').DirectoryTask;
-
-exports.Task = Task;
-exports.FileTask = FileTask;
-exports.DirectoryTask = DirectoryTask;
-
